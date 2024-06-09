f06009d5a565f8f65d7bc0c9e4ecadb5a8117177
Andrey <andrey.volk.1999@gmail.com>
2024-06-09 23:54:41 +1000

    add Solution HW10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Test 1");
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName  =  "Ivanovich";
+        String fullName  =  firstName + " " + middleName + " " + lastName;
+        System.out.println(fullName);
+
+        System.out.println("\n----------------\nTest  2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println("\n----------------\nTest  3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё" , "е");
+        System.out.println(fullName);
     }
 }
